5cdce840affaeb068cfc777310c7aef03a36c769
_Shikyu_ <67348954+Adobe12327@users.noreply.github.com>
2024-12-08 23:40:09 +0900

    Added EquipItemListInfo Packet

---
+++ PROJ_DCCC/HTTP/DTO/EquipItemListInfo.cs
@@ -0,0 +1,62 @@
+﻿using MySql.Data.MySqlClient;
+using PROJ_DCCC.HTTP.DTO.Request;
+
+namespace PROJ_DCCC.HTTP.DTO.Response
+{
+    class RP_EquipItemListInfo : HTTPResponce
+    {
+        public class EquipItemInfo
+        {
+            public string equipItemNokey;
+            public int equipItemNo;
+            public int optionNo1;
+            public int optionNo2;
+            public int reinforceLevel;
+            public string equipItemName;
+            public int saleGoldAmt;
+            public string grade;
+        }
+
+        public EquipItemInfo[] equips;
+        public override void Process(HTTPRequest request)
+        {
+            var req = (RQ_EquipItemListInfo)request;
+            if (Utils.IsVaildToken(req.characterReq.accountSeq, req.characterReq.token))
+            {
+                using (MySqlConnection mysql = new MySqlConnection(Configuration.connStr))
+                {
+                    mysql.Open();
+                    string query = "SELECT * FROM equipitemlist WHERE accountSeq = @accountSeq";
+
+                    var cmd = new MySqlCommand(query, mysql);
+                    cmd.Parameters.Add("@accountSeq", MySqlDbType.Int64).Value = req.characterReq.accountSeq;
+                    var reader = cmd.ExecuteReader();
+                    var itemList = new List<EquipItemInfo>();
+                    while (reader.Read())
+                    {
+                        var item = new EquipItemInfo();
+                        item.equipItemNokey = reader["equipItemNokey"].ToString();
+                        item.equipItemNo = (int)reader["equipItemNo"];
+                        item.optionNo1 = (int)reader["optionNo1"];
+                        item.optionNo2 = (int)reader["optionNo2"];
+                        item.reinforceLevel = (int)reader["reinforceLevel"];
+                        item.equipItemName = (string)reader["equipItemName"];
+                        item.saleGoldAmt = (int)reader["saleGoldAmt"];
+                        item.grade = (string)reader["grade"];
+                        itemList.Add(item);
+                    }
+                    equips = itemList.ToArray();
+                }
+                success = true;
+            }
+        }
+    }
+}
+
+namespace PROJ_DCCC.HTTP.DTO.Request
+{
+    class RQ_EquipItemListInfo : HTTPRequest
+    {
+        public UserReqInfo characterReq;
+    }
+}
--- PROJ_DCCC/HTTP/HTTPPath.cs
+++ PROJ_DCCC/HTTP/HTTPPath.cs
@@ -47,6 +47,7 @@ namespace PROJ_DCCC.HTTP
             { "/user/tire/check/", new DTOs(typeof(RQ_TireCheck), typeof(RP_TireCheck)) },
             { "/tire/present/list/", new DTOs(typeof(RQ_GiftList), typeof(RP_GiftList)) },
             { "/shop/car/unlockbuy/", new DTOs(typeof(RQ_UnLockBuyCar), typeof(RP_UnLockBuyCar)) },
+            { "/user/character/equip/info/", new DTOs(typeof(RQ_EquipItemListInfo), typeof(RP_EquipItemListInfo)) },
         };
     }
 }
